996da952f4b3c66019c1d5f6285a6054faeebc42
donnwilzy <ask4godswill@gmail.com>
2023-09-08 11:51:13 +0100

    routing

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-////// first of all import express
 const express = require('express');
-
-//// dot env (because i am also usin mongo to connect. i am saving my cridentials in .env file)
 const dotenv = require('dotenv');
-
-//// assigning vriable to app
 const app = express();
 
-// app.use(express.json());
 dotenv.config();
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 
 function getCurrentUtcTime() {
     const now = new Date();
     const utcOffsetInMinutes = now.getTimezoneOffset() - (60 * 1); // 1 hour behind WAT
     const utcTime = now;
-  
+
     // Validate the UTC offset
     if (utcOffsetInMinutes < -120 || utcOffsetInMinutes > 120) {
-      throw new Error('The UTC offset must be within +/- 2 hours');
+        throw new Error('The UTC offset must be within +/- 2 hours');
     }
-  
+
     return utcTime;
-  }
-
-
-  // Get the current day of the week
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const currentDay = daysOfWeek[new Date().getDay()];
-
-
-const outPutdata = {
- "slack_name": "GODSWILL EFFIONG",
- "current_day": currentDay,
-  "utc_time": getCurrentUtcTime(),
- "track": "backend",
- "github_file_url": "https://github.com/DONNWILZY/hngTaskOne/blob/master/index.js",
- "github_repo_url": "https://github.com/DONNWILZY/hngTaskOne",
- "status_code": 200
-
- 
 }
-console.log(outPutdata)
 
+// Get the current day of the week
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const currentDay = daysOfWeek[new Date().getDay()];
 
+const outputData = {
+    "slack_name": "GODSWILL_EFFIONG",
+    "current_day": currentDay,
+    "utc_time": getCurrentUtcTime(),
+    "track": "backend",
+    "github_file_url": "https://github.com/DONNWILZY/hngTaskOne/blob/master/index.js",
+    "github_repo_url": "https://github.com/DONNWILZY/hngTaskOne",
+    "status_code": 200
+};
 
-app.get('/api/data', (req, res) => {
-     const slack_name = req.query.slack_name;
+/////// DEFAULT ROUTE
+app.get('/', (req, res) => {
+    res.send(outputData)
+});
+
+/////// ROUTE TO GET QUERIED DATA
+app.get('/api/dataQuery', (req, res) => {
+    const slack_name = req.query.slack_name;
     const track = req.query.track;
 
     // Validate the track parameter
@@ -55,22 +47,30 @@ app.get('/api/data', (req, res) => {
         return res.status(400).json({ error: 'Invalid track' });
     }
 
-    
+    // Validate the slack_name parameter
+    if (slack_name !== 'GODSWILL_EFFIONG') {
+        return res.status(400).json({ error: 'Incorrect slack name' });
+    }
+    // Response data
     res.json({
         status: 200,
-        message: 'sucessfully fetched data',
-        data: outPutdata
+        message: 'Successfully fetched data',
+        data: outputData
+    });
+});
 
-    })
-  });
-
-
-
-
+///// ROUTE TO GET ALL DATA
+app.get('/api/data', (req, res) => {
+        // Response data
+    res.json({
+        status: 200,
+        message: 'Successfully fetched data',
+        data: outputData
+    });
+});
 
 
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-  });
-
+});
